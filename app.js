1962b2fa6a1d9ed611115ccfd15d3adc06e85d4e
Elithon Silva <elithonsilva@gmail.com>
2022-02-04 16:16:18 -0300

    cria arquivo app com os requires necessarios

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app
+  .route('/user')
+  .post('');
+
+module.exports = app;
