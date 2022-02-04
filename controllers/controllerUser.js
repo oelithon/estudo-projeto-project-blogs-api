6fbf0bfa8fbc3fa5e47253f890f704ce50fcf99a
Elithon Silva <elithonsilva@gmail.com>
2022-02-04 17:31:53 -0300

    cria controllerUser.js com retorno de usuario cadastrado e gera um token

---
+++ controllers/controllerUser.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const jwt = require('jsonwebtoken');
+const { User } = require('../models');
+
+const secret = process.env.JWT_SECRET;
+
+module.exports = async (req, res) => {
+  try {
+    const { displayName, email, password, image } = req.body;
+
+    const user = await User.create({ displayName, email, password, image });
+
+    const jwtConfig = {
+      expiresIn: '1d',
+      algorithm: 'HS256',
+    };
+
+    const token = jwt.sign({ username: user.displayName }, secret, jwtConfig);
+
+    res.status(201).json({ token });
+  } catch (error) {
+    res.status(401).json(error.errors);
+  }
+};
