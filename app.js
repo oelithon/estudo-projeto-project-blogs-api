c09c2e2bf9f4614cd03d518ad72a645158ed8997
Elithon Silva <elithonsilva@gmail.com>
2022-02-04 19:08:44 -0300

    faz require do controllerUser

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const controllerUser = require('./controllers/controllerUser');
+
 const app = express();
 
 app.use(bodyParser.json());
 
 app
   .route('/user')
-  .post('');
+  .post(controllerUser);
 
 module.exports = app;
