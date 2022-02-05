0a9cd69108ca575f449475b0c83d01521eda2e36
Elithon Silva <elithonsilva@gmail.com>
2022-02-05 12:27:25 -0300

    add validaçao notNull em displayName e password

--- models/User.js
+++ models/User.js
@@ -1,15 +1,15 @@
 // https://sequelize.org/v6/manual/validations-and-constraints.html
 
-const validateDisplayName = { notEmpty: true, len: [8, 200] };
+const validateDisplayName = { notNull: true, notEmpty: true, len: [8, 200] };
 const validateEmail = { notNull: true, notEmpty: true, isEmail: true };
-const validatePassword = { notEmpty: true, len: [6, 6] };
+const validatePassword = { notNull: true, notEmpty: true, len: [6, 6] };
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    displayName: { type: DataTypes.STRING, validate: validateDisplayName },
+    displayName: { allowNull: false, type: DataTypes.STRING, validate: validateDisplayName },
     email: { allowNull: false, type: DataTypes.STRING, validate: validateEmail },
-    password: { type: DataTypes.STRING, validate: validatePassword },
+    password: { allowNull: false, type: DataTypes.STRING, validate: validatePassword },
     image: DataTypes.STRING,
   },
     {
