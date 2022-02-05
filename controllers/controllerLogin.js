9395d0fe65f04d6a0fb33bbb0007f5644479c19e
Elithon Silva <elithonsilva@gmail.com>
2022-02-05 17:58:53 -0300

    refatora validaçao de email e password

--- controllers/controllerLogin.js
+++ controllers/controllerLogin.js
@@ -2,26 +2,26 @@ require('dotenv').config();
 
 const jwt = require('jsonwebtoken');
 const { User } = require('../models');
-const { requirementsValidation } = require('./validations/validateLogin');
+const { emailAndPasswordValidate } = require('./validations/validateLogin');
 
 const secret = process.env.JWT_SECRET;
 
 module.exports = async (req, res) => {
-  try {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
+  const errors = emailAndPasswordValidate(email, password);
 
-    const user = await User.findOne({ where: { email } });
-
-    const jwtConfig = {
-      expiresIn: '1d',
-      algorithm: 'HS256',
-    };
-
-    const token = jwt.sign({ username: user }, secret, jwtConfig);
-
-    return res.status(200).json({ token });
-  } catch (error) {
-    const { status, message } = requirementsValidation(error);
-    res.status(status).json({ message });
+  if (errors) {
+    return res.status(400).json({ message: errors.message });
   }
+  const user = await User.findAll({ where: { email, password } });
+  if (user.length < 1) return res.status(400).json({ message: 'Invalid fields' });
+
+  const jwtConfig = {
+    expiresIn: '1d',
+    algorithm: 'HS256',
+  };
+
+  const token = jwt.sign({ username: user }, secret, jwtConfig);
+
+  return res.status(200).json({ token });
 };
--- controllers/validations/validateLogin.js
+++ controllers/validations/validateLogin.js
@@ -1,38 +1,26 @@
-const validateEmail = (path, type, validatorKey) => {
-  if (path === 'email' && validatorKey === 'is_null') {
+const validateEmail = (email) => {
+  if (email === '') {
+    return { status: 400, message: '"email" is not allowed to be empty' };
+  }
+  if (!email) {
     return { status: 400, message: '"email" is required' };
   }
-  if (path === 'email' && type === 'Validation error') {
-    return { status: 400, message: '"email" must be a valid email' };
-  }
   return false;
 };
 
-const validateEmailExists = (path, validatorKey) => {
-  if (path === 'Users.email' && validatorKey === 'not_unique') {
-    return { status: 409, message: 'User already registered' };
+const validatePassword = (password) => {
+  if (password === '') {
+    return { status: 400, message: '"password" is not allowed to be empty' };
   }
-};
-
-const validatePassword = (path, validatorKey) => {
-  if (path === 'password' && validatorKey === 'is_null') {
+  if (!password) {
     return { status: 400, message: '"password" is required' };
   }
-  if (path === 'password') {
-    return { status: 400, message: '"password" length must be 6 characters long' };
-  }
   return false;
 };
 
-const requirementsValidation = (error) => {
-  const { path, type, validatorKey } = error.errors[0];
-
-  return (
-    validateEmail(path, type, validatorKey) || validateEmailExists(path, validatorKey) || (
-      validatePassword(path, validatorKey)) || {}
-  );
-};
+const emailAndPasswordValidate = (email, password) =>
+  validateEmail(email) || validatePassword(password) || false;
 
 module.exports = {
-  requirementsValidation,
+  emailAndPasswordValidate,
 };
