3524231d7f416f68306a1a70bbf8ad4e52302c67
Elithon Silva <elithonsilva@gmail.com>
2022-02-05 12:14:00 -0300

    refatora validaçao de displayname

--- controllers/validation.js
+++ controllers/validation.js
@@ -1,6 +1,12 @@
 const requirementsValidation = (error) => {
-  const { path } = error.errors[0];
+  const { path, validatorKey } = error.errors[0];
 
+  if (path === 'displayName' && validatorKey === 'is_null') {
+    return {
+      status: 400,
+      message: '"displayName" is required',
+    };
+  }
   if (path === 'displayName') {
     return {
       status: 400,
